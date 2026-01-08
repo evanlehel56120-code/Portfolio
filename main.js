a7d6c818d9a950435621546e787d1b43c518f00d
evanlehel56120-code <evanlehel56120@gmail.com>
2026-01-08 18:52:21 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+    // Magasin dont les heures d'ouverture sont:
+    8h-12h
+
+    // if (userInput === code) {
+    //  // Le user a tapé le bon code
+    //  console.log("Coffre ouvert");
+    
+    // }
+    // else {
+    //  // Le user s'est trompé de code
+    //  console.log("Coffre fermé");
+    // }
+
+    // function sayHello() {
+    //  console.log("Hello");
+    // }
+
+    let variable =456789
+
+    function addition(number1,number2) {
+        console.log(variable);
+
+        return number1 + number2
+    }
+
+    const result = addition(10,20)
+    console.log(result)
+
+    const chiffre1 = 8
+    const chiffre2 = 12
+
+    console.log(addition(chiffre1, chiffre2))
+
+    const mainTitle = document.querySelector("h1")
+
+    console.log(mainTitle);
